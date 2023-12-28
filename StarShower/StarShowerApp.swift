85ed41b55b178a2cfecc5f3afd629b6e1a5a77f2
eyhdev <alaqsa335@gmail.com>
2023-12-28 15:00:36 +0300

    Initial Commit

---
+++ StarShower/StarShowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarShowerApp.swift
+//  StarShower
+//
+//  Created by eyh.mac on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct StarShowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
