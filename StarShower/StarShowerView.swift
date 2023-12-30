fc9d84b846c2f254016790442ffe99966c7c7070
eyhdev <alaqsa335@gmail.com>
2023-12-30 14:19:20 +0300

    Star Shower Animation

---
+++ StarShower/StarShowerView.swift
@@ -0,0 +1,18 @@
+//
+//  StarShowerView.swift
+//  StarShower
+//
+//  Created by eyh.mac on 28.12.2023.
+//
+
+import SwiftUI
+
+struct StarShowerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StarShowerView()
+}
