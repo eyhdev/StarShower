0dff4eed0183c20e1bbea3121dfb5189212e7486
eyhdev <alaqsa335@gmail.com>
2023-12-30 14:26:45 +0300

    Star Shower Animation

--- StarShower/ContentView.swift
+++ StarShower/ContentView.swift
@@ -1,4 +1,4 @@
-//
+                       //
 //  ContentView.swift
 //  StarShower
 //
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+struct Star{
+    var position: CGPoint
+    var velocity: CGFloat
+}
+
 struct ContentView: View {
+    @State private var stars: [Star] = []
+    
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common)
+        .autoconnect()
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Canvas { context, size in
+            for star in stars {
+                var conterxtCopy = context
+                let rect = CGRect(x: star.position.x, y: star.position.y, width: 2, height: 2)
+                conterxtCopy.fill(Path(ellipseIn: rect), with: .color(.white))
+            }
         }
-        .padding()
+        .background(Color.black)
+        .onAppear{
+            for _ in 0..<100 {
+                let star = Star(position: CGPoint(x: CGFloat.random(in: 0...screenWidth), y: CGFloat.random(in: 0...screenHeight)),
+                                velocity: CGFloat.random(in: 2...5)
+                )
+                stars.append(star)
+            }
+        }
+        .onReceive(timer) { _ in
+            for i in 0..<stars.count {
+                stars[i].position.y += stars[i].velocity
+                if stars[i].position.y > screenHeight {
+                    stars[i].position = CGPoint(x: CGFloat.random(in: 0...screenWidth), y: 0)
+                }
+            }
+        }
     }
 }
 
